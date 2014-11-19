8d177273705e9ccd473e46b198dd8c59ed6b42fc
Adao Pinto <pinto@clipkit.de>
2014-11-19 15:50:32 +0000

    Adding Main class for initialize the application, BaseController for Hello World and base AppConfig to stick standard application configurations

---
+++ src/main/java/com/adaofeliz/Main.java
@@ -0,0 +1,15 @@
+package com.adaofeliz;
+
+import com.adaofeliz.config.AppConfig;
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created on 19/11/14.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AppConfig.class, args);
+    }
+
+}
---
+++ src/main/java/com/adaofeliz/web/controller/BaseController.java
@@ -0,0 +1,19 @@
+package com.adaofeliz.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created on 19/11/14.
+ */
+@Controller
+public class BaseController {
+
+    @ResponseBody
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello World: Spring MVC without XML configuration";
+    }
+}
